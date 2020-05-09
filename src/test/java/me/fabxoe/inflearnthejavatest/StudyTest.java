3c7643937b143be24b4e6cb287b0cbbce310665f
Sungmin Oh <fabxoe.kor@gmail.com>
2020-05-09 18:03:53 +0900

    assuminThat

--- src/test/java/me/fabxoe/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/fabxoe/inflearnthejavatest/StudyTest.java
@@ -8,31 +8,31 @@ import java.util.function.Supplier;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 class StudyTest {
 
     @Test
     @DisplayName("스터디 만들기")
     void create_new_study() {
-        Study actual = new Study(10);
-        assertThat(actual.getLimit()).isGreaterThan(0);
+        String test_env = System.getenv("TEST_ENV");
+        System.out.println(test_env);
 
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
-        assertEquals("limit은 0보다 커야 한다.", illegalArgumentException.getMessage());
-        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
-                new Study(10);
-                Thread.sleep(300);
-                // TODO Preemptively는 ThreadLocal에서 사용시 문제가 생길 수 있다. 스프링 트랜잭션 다른 스레드와 공유가 안되는데. 테스트에서 예상치 못한 문제가 발생할 수 있다.
+//        assumeTrue("LOCAL".equalsIgnoreCase(test_env));
+
+        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
+            System.out.println("local");
+            Study actual = new Study(100);
+            assertThat(actual.getLimit()).isGreaterThan(0);
         });
-//        Study study = new Study(-10);
-//        assertAll(
-//                () -> assertNotNull(study),
-//                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
-//                        () -> "스터디를 처음 만들면 상태값이 " + StudyStatus.DRAFT + "상태다."),
-//                () -> assertTrue(study.getLimit() > 0, ()->"스터디 최대 참석가능 인원은 0보다 커야 한다.")
-//        );
-//        assertNotNull(study);
-//        System.out.println("create");
+
+        assumingThat("sungmin".equalsIgnoreCase(test_env), () -> {
+            System.out.println("sungmin");
+            Study actual = new Study(10);
+            assertThat(actual.getLimit()).isGreaterThan(0);
+        });
+
 
     }
 
