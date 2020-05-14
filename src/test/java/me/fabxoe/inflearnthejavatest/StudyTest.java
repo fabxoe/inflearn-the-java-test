22cd27b8b1988c8b6a2d6f09fa5f364e460e5d8f
Sungmin Oh <fabxoe.kor@gmail.com>
2020-05-14 13:06:43 +0900

    @ValueSource ints

--- src/test/java/me/fabxoe/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/fabxoe/inflearnthejavatest/StudyTest.java
@@ -42,12 +42,13 @@ class StudyTest {
     //반복할 때마다 다른 값을 사용하는 기능 junit5 부터 기본 지원
     @DisplayName("스터디 만들기")
     @ParameterizedTest(name = "{index} {displayName} message={0}") //메서드의 0번 파라미터를 참조. 현재 파라미터가 1개 밖에 없으니 0번 파라미터만 참조하고 있다.
-    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있어요."})
+//    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있어요."})
 //    @EmptySource
 //    @NullSource
 //    @NullAndEmptySource
-    void parameterizedTest(String message) {
-        System.out.println(message);
+    @ValueSource(ints = {10, 20, 40})
+    void parameterizedTest(Integer limit) {
+        System.out.println(limit);
     }
 
     @BeforeAll
