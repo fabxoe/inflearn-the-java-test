e9d285574cbb4de6d2aae9e6907a0bbf6d8ec1ed
Sungmin Oh <fabxoe.kor@gmail.com>
2020-05-14 13:28:14 +0900

    @CsvSource 2개 이상의 argument

--- src/main/java/me/fabxoe/inflearnthejavatest/Study.java
+++ src/main/java/me/fabxoe/inflearnthejavatest/Study.java
@@ -5,6 +5,13 @@ public class Study {
 //    private StudyStatus status = StudyStatus.DRAFT;
     private StudyStatus status;
 
+    private String name;
+
+    public Study(int limit, String name) {
+        this.name = name;
+        this.limit = limit;
+    }
+
     public Study(int limit) {
         if (limit < 0) {
             throw new IllegalArgumentException("limit은 0보다 커야 한다.");
@@ -22,7 +29,22 @@ public class Study {
         return limit;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setLimit(int limit) {
         this.limit = limit;
     }
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "status=" + status +
+                ", name='" + name + '\'' +
+                ", limit=" + limit +
+                '}';
+    }
+
+
 }
--- src/test/java/me/fabxoe/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/fabxoe/inflearnthejavatest/StudyTest.java
@@ -6,10 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.converter.SimpleArgumentConverter;
-import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.NullSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
 import java.lang.reflect.Executable;
 import java.time.Duration;
@@ -45,13 +42,10 @@ class StudyTest {
     //반복할 때마다 다른 값을 사용하는 기능 junit5 부터 기본 지원
     @DisplayName("스터디 만들기")
     @ParameterizedTest(name = "{index} {displayName} message={0}") //메서드의 0번 파라미터를 참조. 현재 파라미터가 1개 밖에 없으니 0번 파라미터만 참조하고 있다.
-//    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있어요."})
-//    @EmptySource
-//    @NullSource
-//    @NullAndEmptySource
-    @ValueSource(ints = {10, 20, 40})
-    void parameterizedTest(@ConvertWith(StudyConverter.class) Study study) {
-        System.out.println(study.getLimit());
+    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+    void parameterizedTest(Integer limit, String name) {
+        Study study = new Study(limit, name);
+        System.out.println(study);
     }
 
     static class StudyConverter extends SimpleArgumentConverter {
